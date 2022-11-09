31f18ca157b37a21338f895fbf482ac5c843279b
onixldlc <onxdl16427@outlook.com>
2022-11-09 13:49:45 +0700

    added docker compose, run script, and how to use

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # hitch-teehee
+simple chatting platform
+
+## run it!
+to run it first allow run.sh to execute using
+```
+chmod u+x run.sh
+```
+
+### to-deploy
+to deploy the app at production ready state, call the run.sh script, and it by default the app will run in production mode
+```
+./run.sh
+```
+
+### to-develop
+to develop the app run it with `DEV` as the argument
+```
+./run.sh DEV
+```
+
+#### ⚠️ NOTE ⚠️ 
+any other value beside `DEV` or `PROD` in the argument would result in running the app in production mode
---
+++ run.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# docker dynamic variables
+SQL_ROOTPASS=$(head -c 24 /dev/random | base64)
+SQL_DBPASS=$(head -c 12 /dev/random | base64)
+SQL_USER="hitchTeehee"
+SQL_PORT=3306
+PMA_PORT=8081
+PORT=8080
+
+# bash script variables
+PROD="docker-prod.yml"
+DEV="docker-dev.yml"
+MODEFILE=".MODE"
+MODE="${1:-PROD}"
+
+# create new random password on .env
+cat > .env << EOM
+SQL_ROOTPASS=${SQL_ROOTPASS}
+SQL_DBPASS=${SQL_DBPASS}
+SQL_USER=${SQL_USER}
+SQL_PORT=${SQL_PORT}
+PMA_PORT=${PMA_PORT}
+PORT=${PORT}
+EOM
+
+# let owner know the password and port
+cat << EOM
+root cred:
+  root:${SQL_ROOTPASS}
+db cred:
+  ${SQL_USER}:${SQL_DBPASS}
+running at port:
+  web: ${PORT}
+  sql: ${SQL_PORT}
+  pma: ${PMA_PORT}
+EOM
+
+
+# function to run and reset docker compose
+function compose () {
+
+  # create temp file to store mode
+  TEMPMODE=$MODE
+  [[ -f "$MODEFILE" ]] && TEMPMODE="$(cat $MODEFILE;)"
+
+  docker-compose -f "$TEMPMODE" down
+  echo "$MODE" > $MODEFILE
+  docker-compose -f "$1" up
+}
+
+
+echo "running docker in: ${MODE} mode"
+YML="$PROD"
+[[ "${MODE}" == "DEV" ]] && YML="$DEV"
+
+compose "$YML"
+

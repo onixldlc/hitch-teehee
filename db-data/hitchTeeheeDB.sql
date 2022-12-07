5ce42ae5f214418def5a70a4bb42f6259965588c
onixldlc <onxdl16427@outlook.com>
2022-12-08 03:00:58 +0700

    new db backup

--- db-data/hitchTeeheeDB.sql
+++ db-data/hitchTeeheeDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysqlDB
--- Generation Time: Nov 14, 2022 at 06:08 PM
+-- Generation Time: Dec 07, 2022 at 07:57 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.25
 
@@ -42,18 +42,8 @@ CREATE TABLE `forum` (
 CREATE TABLE `messages` (
   `userid` varchar(64) NOT NULL,
   `message` text NOT NULL,
-  `timestamp` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `session`
---
-
-CREATE TABLE `session` (
-  `sessionID` varchar(64) NOT NULL,
-  `userID` varchar(64) NOT NULL
+  `timestamp` date NOT NULL,
+  `thread` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -77,7 +67,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`username`, `password`, `creationDate`, `displayname`, `color`, `userid`, `isBanned`) VALUES
-('123', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '2022-11-14 16:53:55', '123', '000000', '242b1620250f7f6f3d0d2d273993ffa91adf39e1f3216ad9b6b43db05ec591bb', 0);
+('admin3', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '2022-12-07 18:23:11', 'admin3', '1c71d8', '143af93018ef02a5c89ffb63a8d4bd03939ba895e383afd65e6e4723bc357e20', 0),
+('123', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '2022-11-14 16:53:55', '123', '000000', '242b1620250f7f6f3d0d2d273993ffa91adf39e1f3216ad9b6b43db05ec591bb', 0),
+('admin2', '96cae35ce8a9b0244178bf28e4966c2ce1b8385723a96a6b838858cdd6ca0a1e', '2022-12-07 11:17:58', 'admin2', '000000', '6dc1bae46c21a4ba2cf0a384a6d1ffde242bd1f25970c8ab1e0b152d07536ffe', 0),
+('admin', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '2022-12-07 11:15:35', 'admin', '000000', 'd601b199a9120309982a5933d109dcfd0f3aaa37394e6a8ef5962d5b2922fc46', 0);
 
 --
 -- Indexes for dumped tables
